34c4d2a1c7bb63b50104a9f77e1afbd1604fb890
Jarek Lipski <loomchild@loomchild.net>
2015-07-14 18:46:37 +0200

    create unit test module

---
+++ python/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+class TestBurstLogging(unittest.TestCase):
+
+    def test_test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
