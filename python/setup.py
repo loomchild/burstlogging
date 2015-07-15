8433478267592685f2316105fcd492995e3ebff5
Jarek Lipski <loomchild@loomchild.net>
2015-07-15 12:57:19 +0200

    bump version to 0.2

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name = "burstlogging",
-    version = "0.1",
+    version = "0.2",
     author = "Jarek Lipski",
     author_email = "pub@loomchild.net",
     description = ("Burst logging library."),
